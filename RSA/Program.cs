92d54c17424a5f75126e30c6f492b01dd01cc09a
Wojciech Grzesiak <wgrzesiak147@gmail.com>
2015-12-25 12:17:25 +0100

    Loading topology from file

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -9,7 +9,10 @@ namespace RSA
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+
+            Topology top = new Topology();
+            top.LoadTopology(@"C:\Users\wgrzesiak147\Downloads\RSA_any_uni_dane\RSA any uni dane\DT14 1.75 Tbps\dd.net");
         }
     }
 }
