c5431b2757c9acb80aaa8f2e32b658a9ee09ca8f
vumipl <196174@student.pwr.edu.pl>
2016-01-06 22:57:23 +0100

    Changed "Weight" to "Distance" in Link class

--- RSA/Entities/Link.cs
+++ RSA/Entities/Link.cs
@@ -5,17 +5,17 @@
         public int Index { get; set; }
         public int StartNode { get; set; }
         public int EndNode { get; set; }
-        public int Weight { get; set; }
+        public int Distance { get; set; }
         public int FreeSlotsNumber { get; set; }
         public int TakenSlotsNumber { get; set; }
         public int SlotsArray { get; set; }
 
-        public Link(int id, int startNode, int endNode, int weight)
+        public Link(int id, int startNode, int endNode, int distance)
         {
             Index = id;
             StartNode = startNode;
             EndNode = endNode;
-            Weight = weight;
+            Distance = distance;
         }
     }
 }
